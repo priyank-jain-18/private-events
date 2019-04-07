7a5afb85d3e81e07ede37ea61a3fd0c8ca280563
Priyank <Priyank.pj@gmail.com>
2019-04-07 09:07:32 +0530

    Login complete with sessions enabled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Private Events App!"
       redirect_to @user
     else
