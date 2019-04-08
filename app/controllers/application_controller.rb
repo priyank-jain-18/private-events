0acf50de2cae62887be096e37962a83f0be01086
Priyank <Priyank.pj@gmail.com>
2019-04-08 12:34:09 +0530

    Added description field and view pages for events and add events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+    def log_in(user)
+    session[:user_id] = user.id
+    current_user
+    end
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+    def log_out
+    session[:user_id] = nil
+    @current_user = nil
+    end
+    private
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
